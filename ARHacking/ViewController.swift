ab13c3329beb7d6d710aa2cad92801e06d7ae2ae
Martin Mitrevski <martin.mitrevski@netcetera.com>
2018-09-08 17:37:57 +0200

    implemented image tracking

--- ARHacking/ViewController.swift
+++ ARHacking/ViewController.swift
@@ -14,62 +14,84 @@ class ViewController: UIViewController, ARSCNViewDelegate {
 
     @IBOutlet var sceneView: ARSCNView!
     
+    static let jackVideo = Bundle.main.url(forResource: "video2", withExtension: "mp4")!
+    static let ballantinesVideo = Bundle.main.url(forResource: "video1", withExtension: "mp4")!
+    let players = [ "jack" : AVPlayer(url: jackVideo),
+                    "ballantines" : AVPlayer(url: ballantinesVideo)]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // Set the view's delegate
-        sceneView.delegate = self
-        
-        // Show statistics such as fps and timing information
-        sceneView.showsStatistics = true
-        
-        // Create a new scene
         let scene = SCNScene(named: "art.scnassets/ship.scn")!
-        
-        // Set the scene to the view
-        sceneView.scene = scene
+        self.sceneView.scene = scene
+        sceneView.delegate = self
+        for (_, player) in players {
+            videoObserver(for: player)
+        }
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        
-        // Create a session configuration
-        let configuration = ARWorldTrackingConfiguration()
-
-        // Run the view's session
-        sceneView.session.run(configuration)
+        setupImageTrackingConfiguration()
     }
     
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
-        
         // Pause the view's session
         sceneView.session.pause()
+    }
+    
+    func setupImageTrackingConfiguration() {
+        let referenceImages = ARReferenceImage.referenceImages(inGroupNamed: "Whiskies", bundle: Bundle.main)!
+        let configuration = ARImageTrackingConfiguration()
+        configuration.trackingImages = referenceImages
+        configuration.maximumNumberOfTrackedImages = 2
+        sceneView.session.run(configuration)
+    }
+    
+    func videoObserver(for videoPlayer: AVPlayer) {
+        NotificationCenter.default.addObserver(forName: NSNotification.Name.AVPlayerItemDidPlayToEndTime,
+                                               object: nil, queue: nil) { notification in
+            videoPlayer.seek(to: CMTime.zero)
+        }
     }
 
     // MARK: - ARSCNViewDelegate
     
-/*
-    // Override to create and configure nodes for anchors added to the view's session.
     func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
         let node = SCNNode()
-     
+        if let imageAnchor = anchor as? ARImageAnchor {
+            let player = players[imageAnchor.name!]!
+            let plane = SCNPlane(width: imageAnchor.referenceImage.physicalSize.width, height: imageAnchor.referenceImage.physicalSize.height)
+            plane.firstMaterial?.diffuse.contents = player
+            player.seek(to: CMTime.zero)
+            player.play()
+            
+            let planeNode = SCNNode(geometry: plane)
+            planeNode.eulerAngles.x = -.pi / 2
+            node.addChildNode(planeNode)
+        }
         return node
     }
-*/
     
-    func session(_ session: ARSession, didFailWithError error: Error) {
-        // Present an error message to the user
-        
+    func renderer(_ renderer: SCNSceneRenderer, didUpdate node: SCNNode, for anchor: ARAnchor) {
+        guard let imageAnchor = anchor as? ARImageAnchor else {
+            return
+        }
+        if let pointOfView = sceneView.pointOfView {
+            let isVisible = sceneView.isNode(node, insideFrustumOf: pointOfView)
+            if isVisible {
+                let player = players[imageAnchor.name!]!
+                if player.rate == 0 {
+                    player.play()
+                }
+            }
+        }
     }
     
-    func sessionWasInterrupted(_ session: ARSession) {
-        // Inform the user that the session has been interrupted, for example, by presenting an overlay
-        
+    deinit {
+        NotificationCenter.default.removeObserver(self)
     }
     
-    func sessionInterruptionEnded(_ session: ARSession) {
-        // Reset tracking and/or remove existing anchors if consistent tracking is required
-        
-    }
 }
